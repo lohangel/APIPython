634c11b6e3220fb364dc55677a3a64957596c39b
lohangel <lohanrafaeel@gmail.com>
2025-07-28 14:33:59 -0300

    subindo novamente

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings 
 
 class Settings(BaseSettings):  
-    supabase_url: str  
-    supabase_key: str 
+    supabase_url:str
+    supabase_key:str
 
     class Config:  
         env_file = ".env"  
